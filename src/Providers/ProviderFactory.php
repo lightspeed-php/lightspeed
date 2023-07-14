7591f279bbf58a5bf7afe24621705218b5569eaa
jradtilbrook <jarryd@tilbrooktech.com>
2023-07-14 19:16:46 +0800

    Add ProviderFactory

---
+++ src/Providers/ProviderFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ThingyClient\Providers;
+
+class ProviderFactory
+{
+    const PROVIDERS = [
+        BuildkiteProvider::class,
+    ];
+
+    public static function resolveProvider()
+    {
+        foreach (self::PROVIDERS as $create) {
+            $provider = new $create;
+            if ($provider->detect()) {
+                return $provider;
+            }
+        }
+
+        return false;
+    }
+}
