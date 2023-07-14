8cf3a9a5fc600157cc82ec7f30d18ae18cc8d090
jradtilbrook <jarryd@tilbrooktech.com>
2023-07-14 16:33:21 +0800

    Add custom TestRunner using the API client

---
+++ src/TestRunner.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ThingyClient;
+
+use PHPUnit\Framework\Reorderable;
+use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestResult;
+use PHPUnit\Framework\TestSuite;
+use RecursiveIteratorIterator;
+
+class TestRunner
+{
+    /**
+     * @var API
+     */
+    private $api;
+
+    /**
+     * @var array<Test|Reorderable>
+     */
+    private $tests = [];
+
+    public function __construct(API $api)
+    {
+        $this->api = $api;
+    }
+
+    public function run(TestSuite $suite, TestResult $result)
+    {
+        // build up a map of test names to suites
+        foreach (new RecursiveIteratorIterator($suite) as $test) {
+            /** @var Reorderable $test */
+            $this->tests[$test->sortId()] = $test;
+        }
+
+        // try connect to an existing build
+        $apiResponse = $this->api->connect();
+
+        if ($apiResponse === false) {
+            // no existing build to join. we need to import one
+            // no tests left in the queue to run
+            $apiResponse = $this->api->import(array_keys($this->tests));
+        }
+
+        $this->recursivelyRun($apiResponse['files'], $result);
+    }
+
+    private function recursivelyRun(array &$tests, TestResult $result)
+    {
+        if (count($tests) > 0) {
+            foreach ($tests as $selectedTest) {
+                $this->tests[$selectedTest]->run($result);
+            }
+
+            $apiResponse = $this->api->queue();
+            $this->recursivelyRun($apiResponse['files'], $result);
+        }
+    }
+}
---
+++ tests/Unit/TestRunnerTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestResult;
+use PHPUnit\Framework\TestSuite;
+use ThingyClient\API;
+use ThingyClient\TestRunner;
+
+class TestRunnerTest extends TestCase
+{
+    public function test_existing_empty_build_doesnt_run_any_tests()
+    {
+        $api = $this->createMock(API::class);
+        // the api client should try connect only once
+        $api->expects($this->once())->method('connect')->willReturn(['files' => []]);
+
+        $runner =  new TestRunner($api);
+        $suite = $this->getMockBuilder(TestSuite::class)
+            ->enableProxyingToOriginalMethods()
+            ->onlyMethods(['run'])
+            ->getMock();
+        // the test suite should never be run because the api responded with no tests
+        $suite->expects($this->never())->method('run');
+        $suite->setTests([]);
+
+        $runner->run($suite, new TestResult);
+    }
+
+    public function test_existing_build_runs_test_until_empty()
+    {
+        $api = $this->createMock(API::class);
+        // the api client should try connect only once
+        $api->expects($this->once())->method('connect')->willReturn(['files' => ['test 1']]);
+        $api->expects($this->exactly(2))->method('queue')->willReturn(['files' => ['test 2']], ['files' => []]);
+
+        $suite = new TestSuite();
+        $testCase = fn ($name) => new class($name) extends TestCase {
+            public function sortId(): string {
+                return $this->getName();
+            }
+        };
+        $suite->addTest($testCase('test 1'));
+        $suite->addTest($testCase('test 2'));
+        $this->assertCount(2, $suite->tests());
+
+        $runner =  new TestRunner($api);
+
+        $runner->run($suite, new TestResult);
+    }
+
+    public function test_new_empty_build_doesnt_run_any_tests()
+    {
+        $api = $this->createMock(API::class);
+        // the api client should try connect only once
+        $api->expects($this->once())->method('connect')->willReturn(false);
+        // and import only once but return no files
+        $api->expects($this->once())->method('import')->willReturn(['files' => []]);
+
+        $runner =  new TestRunner($api);
+        $suite = $this->getMockBuilder(TestSuite::class)
+            ->enableProxyingToOriginalMethods()
+            ->onlyMethods(['run'])
+            ->getMock();
+        // the test suite should never be run because the api responded with no tests
+        $suite->expects($this->never())->method('run');
+        $suite->setTests([]);
+
+        $runner->run($suite, new TestResult);
+    }
+
+    public function test_new_build_runs_returned_tests_until_empty()
+    {
+        $api = $this->createMock(API::class);
+        // the api client should try connect only once
+        $api->expects($this->once())->method('connect')->willReturn(false);
+        // and call import only once with some tests
+        $api->expects($this->once())->method('import')->willReturn(['files' => ['test 1', 'test 2']]);
+        // and call queue twice returning some tests once and none the next time
+        $api->expects($this->exactly(2))->method('queue')->willReturn(['files' => ['test 3']], ['files' => []]);
+
+        $suite = new TestSuite();
+        $testCase = fn ($name) => new class($name) extends TestCase {
+            public function sortId(): string {
+                return $this->getName();
+            }
+        };
+        $suite->addTest($testCase('test 1'));
+        $suite->addTest($testCase('test 2'));
+        $suite->addTest($testCase('test 3'));
+        $this->assertCount(3, $suite->tests());
+
+        $runner =  new TestRunner($api);
+
+        $runner->run($suite, new TestResult);
+    }
+
+    public function test_dependency_order_works_correctly()
+    {
+        $this->markTestSkipped('Need to implement a test to confirm the @depends annotation works');
+    }
+
+    public function test_dataprovider_works_correctly()
+    {
+        $this->markTestSkipped('Need to implement a test to confirm the @dataProvider annotation works');
+    }
+}
