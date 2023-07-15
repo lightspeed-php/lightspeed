62d7183b0c6bde7f413e3d494ab05be65fd9b092
jradtilbrook <jarryd@tilbrooktech.com>
2023-07-15 18:14:56 +0800

    Debugging

--- src/API.php
+++ src/API.php
@@ -39,6 +39,7 @@ class API
         curl_setopt_array($this->curl, $opts);
 
         $body = curl_exec($this->curl);
+        throw new Exception(json_encode(curl_getinfo($this->curl)));
 
         $this->handleErrors();
 
@@ -48,7 +49,6 @@ class API
 
         $json = json_decode($body, true);
         if (json_last_error() !== JSON_ERROR_NONE) {
-            throw new Exception(curl_error($this->curl));
             throw new Exception(sprintf('Failed to decode json response body. Response: %s', $body));
         }
 
